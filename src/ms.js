f7cae2984564b650a9aa054df58f6604d88c59ca
nwa <nwa.bio.web@gmail.com>
2023-06-23 13:29:07 +0300

    main

---
+++ src/ms.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ms = void 0;
+var ms = /** @class */ (function () {
+    function ms(bucket, seconds) {
+        this.bucket = bucket;
+        this.seconds = seconds;
+    }
+    return ms;
+}());
+exports.ms = ms;
